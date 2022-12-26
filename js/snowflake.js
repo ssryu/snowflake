594368edb392f4067b3638d7113c8825efe5ea36
ssryu <jjkztok3@gmail.com>
2022-12-26 14:52:03 +0900

    Randomize snowflake size

--- js/snowflake.js
+++ js/snowflake.js
@@ -1,5 +1,6 @@
 const body = document.querySelector("body");
 const MIN_ANIMATION_DURATION = 10;
+const MIN_SIZE = 5;
 const SNOWFLAKE_WIDTH = 8;
 const MAX_SNOWFLAKE_LEFT = window.screen.width - 2 * SNOWFLAKE_WIDTH;
 
@@ -8,9 +9,12 @@ function makeSnowflake() {
   const delay = Math.random() * 10;
   const initialOpacity = Math.random();
   const duration = Math.random() * 20 + MIN_ANIMATION_DURATION;
+  const size = Math.random() * 10 + MIN_SIZE;
 
   snowflake.classList.add("snowflake");
   snowflake.style.left = `${Math.min(Math.random() * window.screen.width, MAX_SNOWFLAKE_LEFT)}px`;
+  snowflake.style.width = `${size}px`;
+  snowflake.style.height = `${size}px`;
   snowflake.style.animationDelay = `${delay}s`;
   snowflake.style.opacity = initialOpacity;
   snowflake.style.animation = `fall ${duration}s linear`;
